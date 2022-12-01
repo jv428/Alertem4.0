0028e568dcd633afdeaf9d5ff6be63d2e2d914c4
ronaldtellez <storeraat@gmail.com>
2022-12-01 09:54:00 -0500

    correcciones de la tblAsistencia

--- Vistas/mostrar_asistencia.php
+++ Vistas/mostrar_asistencia.php
@@ -5,7 +5,7 @@
 <?php
     // require_once("/xampp/htdocs/Alertem/Vistas/Comunes/nav.php");
     include_once("/xampp/htdocs/Alertem4.0/Modelo/modelo_asistencia.php");
-    $asistencia = new asistencia(null,null,null,null);
+    $asistencia = new asistencia(null,null,null,null,null);
     $asistencia1 = json_decode($asistencia->listarTabla())   ;
     
 ?>
@@ -30,7 +30,9 @@
                     <th>Asistencia</th>
                     <th>Fecha</th>
                     <th>Hora</th>
+                    <th>Asignatura</th>
                     <th>Estudiante</th>
+                    <th>Grupo</th>
                     <th style="width: 120px;">Acciones</th>
                 </tr>
             </thead>
@@ -47,7 +49,9 @@
                         };?></td>
                         <td><?php echo $dato->fecha_as; ?></td>
                         <td><?php echo $dato->hora_as; ?></td>
-                        <td><?php echo $dato->documento_us; ?> <?php echo $dato->nombre_us; ?> <?php echo $dato->p_apellido_us; ?> <?php echo $dato->s_apellido_us; ?></td>                        
+                        <td><?php echo $dato->descripcion_as; ?></td>
+                        <td><?php echo $dato->documento_us; ?> <?php echo $dato->nombre_us; ?> <?php echo $dato->p_apellido_us; ?> <?php echo $dato->s_apellido_us; ?></td>
+                        <td><?php echo $dato->descripcion_gr; ?></td>                        
                         <td><button class="btn_tabla" ><a href="../Vistas/asistencia.php?id_asi=<?php echo($dato->id_asi)?>"><i class="fa-solid fa-user-pen"></i>  Editar</a> 
                             </button> 
                             <button class="btn_tabla"><i class="fa-solid fa-trash-can"></i>
